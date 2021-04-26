d832bbc59414a1166f6f8789f85a75f276fee565
Stephen Carey <scarey8@uncc.edu>
2021-04-26 17:44:24 -0400

    Redid api call to utilize NutritionX and added search/select functionality

--- APIManager.js
+++ APIManager.js
@@ -1,32 +1,47 @@
-const url = "https://nutrition-api.esha.com/foods?";
+let selected = [];
+let responses = [];
 
-function searchItem() {
+function foodSearch() {
+    let term =$.trim($("#foodSearch").val());
+    var temphtml;
+    var output = 
+        '<div id="modal" class="modal">' + 
+        '   <span class="close"&times;</span>';
+    responses = [];
 
-    $(function() {
-        var params = {
-            // Request parameters
-            "query": "{string}",
-            "start": "0",
-            "count": "25",
-            "spell": "true",
-        };
-      
-        $.ajax({
-            url: "https://nutrition-api.esha.com/foods?" + $.param(params),
-            beforeSend: function(xhrObj){
-                // Request headers
-                xhrObj.setRequestHeader("Accept","application/json");
-                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","dc8e8289462e42f991b651a6eb973145");
-            },
-            type: "GET",
-            // Request body
-            data: "{body}",
-        })
-        .done(function(data) {
-            alert("success");
-        })
-        .fail(function() {
-            alert("error");
+
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://nutritionix-api.p.rapidapi.com/v1_1/search/" + term + "?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "5e7fa4be77mshf5cf29e213fbd5fp121ee6jsn1bf1a62f3120",
+            "x-rapidapi-host": "nutritionix-api.p.rapidapi.com"
+        }
+    };
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+
+        $.each(response['hits'], function(i, item) {
+            responses.push(item);
+
+            temphtml = 
+                '<p>' + item['fields']['item_name'] + '&emsp;' + 
+                '<button class="btn btn-lg btn-success" style="height:24px;width:75px;" type="submit" onclick="selectItem(' + i + ')">Select</button>'
+                + '</p>';
+            output += temphtml;
         });
+
+        output += '</div>';
+
+        $('#searchs').html(output);
     });
+}
+
+function selectItem(i) {
+    selected.push(responses[i]);
+
+    console.log(selected);
 }
